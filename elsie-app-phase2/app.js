d349e90fc8182e034da31b67496db5d654edb00e
TheGotoFam <bgb4f9k2mf@privaterelay.appleid.com>
2025-11-28 20:59:56 -0600

    Add files via upload

---
+++ elsie-app-phase2/app.js
@@ -0,0 +1,2 @@
+document.getElementById("assistant").onclick=()=>{document.getElementById("assistantBox").style.display='block';};
+function closeAssistant(){document.getElementById("assistantBox").style.display='none';}
